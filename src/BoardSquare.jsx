6e0ac7928ed679eef413a118490bf69ede90814b
Reedham Pujara <reedhamjpujara2092001@gmail.com>
2021-10-29 09:57:51 +0530

    some code written

--- src/BoardSquare.jsx
+++ src/BoardSquare.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Square from "./Square.jsx"
 import Piece from "./Piece.jsx";
 import {useDrop} from 'react-dnd'
-import {move} from './Game'
+import {handleMove} from './Game'
+import { gameSubject } from "./Game";
+import Promote from "./Promote.jsx";
 
 export default function BoardSquare({piece, black, position}) {
-    const [ , drop] = useDrop({
+    const [promotion, setPromotion] = useState(null)
+     const [ , drop] = useDrop({
         accept: 'piece',
         drop: (item) => {
             const [fromPosition] = item.id.split('_')
-            move(fromPosition, position, item.id.split('_')[2])}
+            handleMove(fromPosition, position, item.id.split('_')[2])}
     })
+    useEffect(() => {
+        const subscribe = gameSubject.subscribe(({pendingPromotion}) => 
+            pendingPromotion && pendingPromotion.to === position ? setPromotion(pendingPromotion) : setPromotion(null) 
+        ) 
+        return() => subscribe.unsubscribe()
+    },[])
 
     return (
         <div className="board-square" ref={drop}>
             <Square black={black}>
-                {piece && <Piece piece={piece} position={position} />}
+                {promotion ? <Promote promotion={promotion}/> : piece ? <Piece piece={piece} position={position} /> : null} 
+                
             </Square>
         </div>
     )
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,9 @@
 import * as Chess from "chess.js";
 import {BehaviorSubject} from 'rxjs';
 
-let promotion = 'rnb2nr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
+let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
 
-const chess = new Chess();
+const chess = new Chess( );
 
 export const gameSubject = new BehaviorSubject()
 
@@ -24,9 +24,26 @@ export function initGame() {
     updateGame()
 }
 
-export function move(from, to, x) {
+export function handleMove(from, to,x) {
+    const promotions = chess.moves({verbose: true}).filter(m => m.promotion)
+    console.table(promotions)
+    if(promotions.some(p => `${p.from}:${p.to}` === `${from}:${to}`)) {
+        const pendingPromotion = {from, to, color:promotions[0].color}
+        updateGame(pendingPromotion)
+    }
+    const {pendingPromotion} = gameSubject.getValue()
+    if(!pendingPromotion){
+        move(from,to,x)
+    }
+}
 
-    const legalMove = chess.move({from,to})
+export function move(from, to, x, promotion) {
+    let tempMove = {from,to}
+    if(promotion) {
+        tempMove.promotion = promotion
+    }
+
+    const legalMove = chess.move(tempMove)
 
     if(legalMove) {
         updateGame()
@@ -34,9 +51,9 @@ export function move(from, to, x) {
     }
 }
 
-function updateGame() {
+function updateGame(pendingPromotion) {
     const newGame = {
-        board: chess.board()
+        board: chess.board(), pendingPromotion
     }
     gameSubject.next(newGame)
 }
